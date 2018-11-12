ba7d074d4f0002782d274e0c042786d1e5ffe6f1
Allison Howard <alliemhow@gmail.com>
2018-11-12 08:43:09 +0000

    automatically backed up by learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -1,11 +1,11 @@
 module Dance
   def twirl
-  "I'm twirling!"
-end
+    "I'm twirling!"
+  end
 
-def jump
-  "Look how high I'm jumping!"
-end
+  def jump
+    "Look how high I'm jumping!"
+  end
 
 def pirouette
   "I'm doing a pirouette"
