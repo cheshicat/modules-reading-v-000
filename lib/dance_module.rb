0fcbb28302f021fcd6aeb86fee74ac6f4d1b1baf
Allison Howard <alliemhow@gmail.com>
2018-11-12 08:43:21 +0000

    automatically backed up by learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -7,9 +7,9 @@ module Dance
     "Look how high I'm jumping!"
   end
 
-def pirouette
-  "I'm doing a pirouette"
-end
+  def pirouette
+    "I'm doing a pirouette"
+  end
 
 def take_a_bow
   "Thank you, thank you. It was a pleasure to dance for you all."
